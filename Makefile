26cdc1ed49541bbbba4ba668d2bc0763a8db6ea4
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2022-04-06 18:12:46 +0200

    Should fix tests on CI
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- Makefile
+++ Makefile
@@ -72,6 +72,9 @@ docs: ## generate Sphinx HTML documentation, including API docs
 	$(MAKE) -C docs html
 	$(BROWSER) docs/_build/html/index.html
 
+doctest:
+	$(MAKE) -C docs doctest
+
 servedocs: docs ## compile the docs watching for changes
 	watchmedo shell-command -p '*.rst' -c '$(MAKE) -C docs html' -R -D .
 
--- tests/test_pyistp.py
+++ tests/test_pyistp.py
@@ -6,9 +6,12 @@ import unittest
 from ddt import ddt, data, unpack
 
 from pyistp import ISTPLoader
+import os
+
+current_path = os.path.dirname(__file__)
 
 test_data = (
-    ("resources/wi_k0_mfi_20220101_v01.cdf", [
+    (f"{current_path}/resources/wi_k0_mfi_20220101_v01.cdf", [
         "PGSM", "BGSEc", "BF1", "PGSE", "N", "MODE", "BGSMa", "Time_PB5", "DIST", "BGSEa", "BGSMc", "RMS", "DQF",
         "Gap_Flag"]),
 )
