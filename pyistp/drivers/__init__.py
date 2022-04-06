8a899913d66100a9bfd0249b6a547f22a89f4f7d
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2022-04-06 09:57:54 +0200

    Makes more verbose cdf driver loading
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- pyistp/drivers/__init__.py
+++ pyistp/drivers/__init__.py
@@ -1,9 +1,15 @@
+import logging
+
+log = logging.getLogger(__name__)
+
 try:
     import pycdfpp
     from .pycdfpp import Driver as current_driver
+    log.info("Using pycdfpp")
 except ImportError:
     try:
         import spacepy.pycdf as cdf
         from .spacepy import Driver as current_driver
+        log.info("Using spacepy.pycdf")
     except ImportError:
-        pass
+        raise ImportError("can't import any compatible CDF library")
